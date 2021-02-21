ca029a3a716447d88c9591a9a1167cb34eaf5592
SureshBijande <sureshbijande@gmail.com>
2021-02-21 14:02:46 +0530

    Suresh added first file

---
+++ Sum of two numbers.py
@@ -0,0 +1,13 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def solveMeFirst(a,b):
+	sum = a+b;
+	return sum;
+
+>>> Num1 = int(input("Enter the first value: "))
+Enter the first value: 50
+>>> Num2 = int(input("Enter the second value: "))
+Enter the second value: 40
+>>> print("Sum of two numbers",solveMeFirst(Num1,Num2))
+Sum of two numbers 90
+>>> 
